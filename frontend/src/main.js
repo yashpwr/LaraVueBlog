a5686e040f070a2a5c98bf026892392426a8ec84
unknown <yashp.albiorix@gmail.com>
2022-12-01 10:44:06 +0530

    Setup Api path in main.js using axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 import './assets/main.css'
+
+axios.defaults.baseURL = 'http://localhost:8000/api/';
+axios.defaults.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+
 
 const app = createApp(App)
 
